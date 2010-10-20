4fe94ac83a66ae0f50a3fbf2df8ae04a53c028ed
Ansgar Jazdzewski <aj@radiotux.de>
2010-10-20 08:21:57 +0200

    init

---
+++ searchform.php
@@ -0,0 +1,13 @@
+<?php 
+ /**
+ * The template for displaying Searchform.
+ *
+ * @package WordPress
+ * @subpackage RadioTux
+ * @since RadioTux 2.0
+ */
+?>
+<form method="get" id="searchform" action="<?php bloginfo('home'); ?>/">
+<input type="text" size="50" value="<?php _e('suchen','notepad-theme'); ?>" name="s" id="s" onblur="if (this.value == '') {this.value = '<?php _e('suchen','notepad-theme'); ?>';}" onfocus="if (this.value == '<?php _e('suchen','notepad-theme'); ?>') {this.value = '';}" />
+<input type="hidden" id="searchsubmit" />
+</form> 
